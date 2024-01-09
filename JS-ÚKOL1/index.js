fdf79f39990166f9d987f6a3e68088625b317214
Andriyyj <144383057+Andriyyj@users.noreply.github.com>
2024-01-09 16:53:56 +0100

    Update index.js

--- JS-ÚKOL1/index.js
+++ JS-ÚKOL1/index.js
@@ -1,4 +1,14 @@
-function vytvoritTabulku() {
-    let pocetRadku = document.getElementById("pocet-radku");
-    let pocetSloupcu = document.getElementById("pocet-sloupcu");
+function vytvoritTabulku () {
+
+    var pocetRadku = document.getElementById("pocet-radku").value;
+    var pocetSloupcu = document.getElementById("pocet-sloupcu").value;
+    var tabulka = document.createElement("table");
+    for (var i = 0; i < pocetRadku; i++) {
+        var radek = tabulka.insertRow(i);
+        for (var j = 0; j < pocetSloupcu; j++) {
+            var bunka = radek.insertCell(j);
+            bunka.textContent = (i * pocetSloupcu + j + 1);
+        }
+    }
+    document.body.appendChild(tabulka);
 }
