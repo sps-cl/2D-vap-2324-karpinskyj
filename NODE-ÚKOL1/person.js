e90e0be7a2f3cadaf6ebb856e8cdae8769c8b753
Andriyyj <144383057+Andriyyj@users.noreply.github.com>
2024-03-05 15:55:52 +0100

    Add files via upload

---
+++ NODE-ÚKOL1/person.js
@@ -0,0 +1,8 @@
+class Person {
+    constructor(firstname, surname, id) {
+        this.firstname = firstname;
+        this.surname = surname;
+        this.id = id;
+    }
+}
+module.exports = Person
