53e3894516429aa4fed4c0b99002af5aa4569604
Andriyyj <144383057+Andriyyj@users.noreply.github.com>
2024-03-05 16:06:52 +0100

    Update app.js

---
+++ NODE-ÚKOL1/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const Person = require('./person');
+
+const app = express();
+app.listen(7707);
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/random-person', (req, res) => {
+  const randomPerson = new Person(
+    getRandomName(),
+    getRandomSurname(),
+    generateRandomId()
+  );
+
+  res.json(randomPerson);
+});
+
+function getRandomName() {
+  const names = ["Petr", "David", "Tadeáš", "Philip", "Andrew", "James", "John", ];
+  return names[Math.floor(Math.random() * names.length)];
+}
+
+function getRandomSurname() {
+    const names = ["Novák", "Svoboda", "Černý", "Bílý", "", ""];
+    return names[Math.floor(Math.random() * names.length)];
+  }
+
+function generateRandomId() {
+  return Math.floor(Math.random() * 1000);
+}
