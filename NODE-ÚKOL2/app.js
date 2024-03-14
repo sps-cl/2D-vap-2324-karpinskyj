1f65fa7ed07b1a0a2232eb66efb73ea6ab9c6973
Andriyyj <144383057+Andriyyj@users.noreply.github.com>
2024-03-14 13:39:44 +0100

    Update app.js

--- NODE-ÚKOL2/app.js
+++ NODE-ÚKOL2/app.js
@@ -1,36 +1,29 @@
-const mysql2 = require("mysql2");
-const express = require("express");
-const path = require("path");
+const mysql2 = require('mysql2');
+const express = require('express');
+const path = require('path');
 
-const connection = mysql2.createConnection(
+const conection = mysql2.createConnection(
     {
-        host: "127.0.0.1",
-        user: "root",
-        password: "root",
-        port: "3306",
-        database: "moje_db"
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        port: '3306',
+        database: 'moje_db'
     }
 );
 
-connection.connect((error) => {
-    if (error) {
+conection.connect((error) => {
+    if(error){
         console.log(error)
     } else {
-        console.log("Úspěšně připojeno.");
-        const jmeno = "Andrej";
-        const prijmeni = "Karpinskyj";
-        connection.query(
-            `INSERT INTO uzivatele(jmeno, prijmeni) VALUES('${jmeno}', '${prijmeni}');`
-        , (error, data) => {
-            if (error) console.log(error);
-        });
-        connection.query(
-            'SELECT * FROM uzivatele;', (error, data) => {
+        console.log("Úspěšně připojeno!");
+        conection.query(
+            'SELECT * FROM uzivvatel;', (error, data) => {
                 if (error) console.log(error);
                 else {
                     console.log(data);
                 }
-            }
+            }    
         )
     }
 });
@@ -40,13 +33,17 @@ app.use(express.static(__dirname));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get("/", (reg, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+})
 
 app.post("/uloz-uzivatele", (req, res) => {
-    console.log(req.body);
-    res.redirect("/");
-});
+    conection.query(
+        `INSERT INTO uzivvatel(jmeno, prijmeni) VALUES('${req.body.jmeno}', '${req.body.prijmeni}');`
+    , (error, data) => {
+        if (error) console.log(error)
+    });
+    res.redirect("/")
+})
 
 app.listen(5500);
