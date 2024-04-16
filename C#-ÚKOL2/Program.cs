eb62ddb96a60c1e2ba4efa281ebbb6271baabf1b
Andriyyj <144383057+Andriyyj@users.noreply.github.com>
2024-04-16 16:49:16 +0200

    Update Program.cs

--- C#-ÚKOL2/Program.cs
+++ C#-ÚKOL2/Program.cs
@@ -1,1 +1,96 @@
+using System;
+using System.Linq;
 
+namespace ConsoleApp2
+{
+    public class ComplexNumber
+    {
+        public double Real;
+        public double Imaginary;
+        public ComplexNumber(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        public ComplexNumber Add(ComplexNumber other)
+        {
+            if (other != null)
+            {
+                return new ComplexNumber(this.Real + other.Real, this.Imaginary + other.Imaginary);
+            }
+            else
+            {
+                Console.WriteLine("nesmi byt prazdny");
+                return null;
+            }
+        }
+
+        public ComplexNumber Subtract(ComplexNumber other)
+        {
+            if (other != null)
+            {
+                return new ComplexNumber(this.Real - other.Real, this.Imaginary - other.Imaginary);
+            }
+            else
+            {
+                Console.WriteLine("nesmi byt prazdny");
+                return null;
+            }
+        }
+
+        public ComplexNumber Multiply(ComplexNumber other)
+        {
+            if (other != null)
+            {
+                return new ComplexNumber((this.Real * other.Real - this.Imaginary * other.Imaginary), (this.Real * other.Real + this.Imaginary * other.Imaginary));
+            }
+            else
+            {
+                Console.WriteLine("nesmi byt prazdny");
+                return null;
+            }
+        }
+
+        public ComplexNumber Divide(ComplexNumber other)
+        {
+            if (other != null)
+            {
+                return new ComplexNumber(((this.Real * other.Real + this.Imaginary * other.Imaginary) / (other.Real * other.Real + other.Imaginary * other.Imaginary)), ((this.Imaginary * other.Real - this.Real * other.Imaginary) / (other.Real * other.Real + other.Imaginary * other.Imaginary)));
+            }
+            else
+            {
+                Console.WriteLine("nesmi byt prazdny");
+                return null;
+            }
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej první komplexní číslo");
+            Console.Write("Reálna část prvního čísla: ");
+            double real1 = double.Parse(Console.ReadLine());
+            Console.Write("Imaginarní část prvního čísla: ");
+            double imaginary1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nZadej druhý komplexní číslo");
+            Console.Write("Realná část druhého čísla: ");
+            double real2 = double.Parse(Console.ReadLine());
+            Console.Write("Imaginarní část druhého čísla: ");
+            double imaginary2 = double.Parse(Console.ReadLine());
+
+            ComplexNumber complex1 = new ComplexNumber(real1, imaginary1);
+            ComplexNumber complex2 = new ComplexNumber(real2, imaginary2);
+
+            ComplexNumber soucet = complex1.Add(complex2);
+            ComplexNumber rozdil = complex1.Subtract(complex2);
+
+            Console.WriteLine($"\nSoučet: {soucet.Real} + {soucet.Imaginary}i");
+            Console.WriteLine($"Rozdíl: {rozdil.Real} + {rozdil.Imaginary}i");
+            Console.ReadLine();
+        }
+    }
+}
